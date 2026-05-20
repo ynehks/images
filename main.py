e146e3d1043b0e5156c650db46fcfaf6c5d9d773
ynehks <fleshenskylox@vk.com>
2026-05-21 00:04:25 +0300

    настройка папок и фильтрация изображений
    Create output folder and list image files in input folder.

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ import csv
 INPUT_FOLDER = "images"
 OUTPUT_FOLDER = "results"
 
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+images = []
+for file in os.listdir(INPUT_FOLDER):
+    if file.endswith(".jpg") or \
+       file.endswith(".png") or \
+       file.endswith(".jpeg"):
+        images.append(file)
+if len(images) == 0:
+    print("Изображения не найдены")
+    exit()
