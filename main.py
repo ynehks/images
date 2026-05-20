dd37e48768967d3b9c64752fe10a6155d8601647
ynehks <fleshenskylox@vk.com>
2026-05-21 00:05:46 +0300

    предобработка изображения и детекция контуров

--- main.py
+++ main.py
@@ -33,4 +33,18 @@ for image_name in images:
         print("Ошибка загрузки")
         continue
     image = cv2.resize(image, (800, 600))
-    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+    edges = cv2.Canny(blur, 50, 150)
+    contours, _ = cv2.findContours(
+        edges,
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+    result = image.copy()
+    count = 0
+    round_objects = 0
+    rectangle_objects = 0
